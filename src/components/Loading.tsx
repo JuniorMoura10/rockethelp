825467fc14df71eb19aeddc61101c2b2f9fe2670
Júnior Moura <geraldomartins10@gmail.com>
2022-07-21 19:20:39 -0300

    primeira aula do ignite-lab RN

---
+++ src/components/Loading.tsx
@@ -0,0 +1,9 @@
+import { Center, Spinner } from 'native-base'
+
+export function Loading(){
+    return(
+        <Center>
+            <Spinner flex={1} bg="gray.700" />
+        </Center>
+    );
+}
